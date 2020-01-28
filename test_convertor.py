cd4113b22b123b92a3b223c61c34c5beca2f46c4
noracnr <823411732@qq.com>
2020-01-27 21:21:12 -0500

    more tests

--- test_convertor.py
+++ test_convertor.py
@@ -2,7 +2,19 @@ import pytest
 import convertor
 
 def test_answer():
-  arabic = [1,2,3,4,78,420,1939]
-  romans = ['I','II','III','IV','LXXVIII','CDXX','MCMXXXIX']
+  arabic = [1,2,3,4,78,420,1939, 3999]
+  romans = ['I','II','III','IV','LXXVIII','CDXX','MCMXXXIX', 'MMMCMXCIX']
   for a, r in zip(arabic,romans):
     assert convertor.convert(a) == r
+
+def test_exception1():
+  arabic = [-1, 0, -32154356]
+  expections1 = "Please enter a number greater than 0"
+  for a in arabic:
+    assert convertor.convert(a) == expections1
+
+def test_exception2():
+  arabic = ["a",'748291']
+  expections2 = "Please enter a integer"
+  for a in arabic:
+    assert convertor.convert(a) == expections2
